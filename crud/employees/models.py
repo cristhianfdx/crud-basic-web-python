ea1b1328de220823fb4e3295b350889727907956
Cristhian Forero <cristhian.forero.dominguez@gmail.com>
2020-05-14 01:32:37 -0500

    Se agrega aplicacion cliente

--- crud/employees/models.py
+++ crud/employees/models.py
@@ -6,6 +6,7 @@ class Employee(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     document_number = models.CharField(max_length=20, unique=True)
+    birth_date = models.DateField(null=True)
     mobile_number = models.CharField(max_length=40, unique=True)
     avatar = models.CharField(max_length=250)
     created = models.DateTimeField(auto_now_add=True)
