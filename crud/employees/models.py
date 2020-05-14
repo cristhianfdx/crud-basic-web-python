2fedcbbdbe9cf763884660c5853d8d3b0bfee442
Cristhian Forero <cristhian.forero.dominguez@gmail.com>
2020-05-13 23:53:23 -0500

    se agrega proyecto Django

---
+++ crud/employees/models.py
@@ -0,0 +1,18 @@
+"""Employee Model."""
+
+from django.db import models
+
+class Employee(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    document_number = models.CharField(max_length=20, unique=True)
+    mobile_number = models.CharField(max_length=40, unique=True)
+    avatar = models.CharField(max_length=250)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'employee'
+
+    def __str__(self):
+        return self.first_name
---
+++ crud/router.py
@@ -0,0 +1,5 @@
+from .employees.viewsets import EmployeeViewset
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('employee', EmployeeViewset)
